ae52fb81032bbeb50d6168281f999214387e9253
JKRT <johti17@liu.se>
2019-11-08 00:40:32 +0100

    Fixed missing export

--- src/ImmutableList.jl
+++ src/ImmutableList.jl
@@ -37,7 +37,7 @@ using .ListDef
 struct ImmutableListException <: Exception end
 
 export List, list, Nil, nil, Cons, cons, @do_threaded_for, <|
-export _cons, ImmutableListException
+export _cons, ImmutableListException, @list
 
 include("util.jl")
 
