c43cb6d1b804cf5c2756cbb66400ae73f2fd17fc
John <jtinnerholm@gmail.com>
2025-01-30 16:01:00 +0100

    Added compile time list generation
    Adjusted merge error

--- src/util.jl
+++ src/util.jl
@@ -1,4 +1,3 @@
-
 """ Throws ImmutableListFailure """
 function listFail()
   throw(ImmutableListException("listFail"))
@@ -7,26 +6,53 @@ end
 
 """
   Append with both lists nil is nil
+```
+listAppend(lst1::Nil, lst2::Nil)
+```
 """
 function listAppend(lst1::Nil{T}, lst2::Nil) where {T}
   nil
 end
 
 """
-  Append when lst1 is a cons and lst 2 is nil is lst1
+  Appending when lst1 is a cons and lst2 is nil is lst1
+```
+listAppend(lst1::Cons{T}, lst2::Nil) where {T}
+```
 """
 function listAppend(lst1::Cons{T}, lst2::Nil) where {T}
   lst1
 end
 
 """
-  Append when lst2 is a cons cell and lst 1 is nil is lst1
+  Append when lst2 is a cons cell and lst1 is nil is lst2
+```
+listAppend(lst1::Nil, lst2::Cons{T}) where {T}
+```
 """
 function listAppend(lst1::Nil, lst2::Cons{T}) where {T}
   lst2
 end
 
-""" O(length(lst1)), O(1) if either list is empty.. needs improvment """
+"""
+```
+listAppend(lst1::Cons{T}, lst2::Cons{T}) where {T}
+```
+
+O(length(lst1)), O(1) if either list is empty..
+
+Example:
+
+```
+julia> listAppend(list(1,2,3), list(4,5,6))
+julia> list{Int64}[1,2,3,4,5,6]
+julia> listAppend(list(1,2), list())
+julia> list{Int64}[1,2]
+```
+
+TODO:
+Optimize me:)
+"""
 function listAppend(lst1::Cons{T}, lst2::Cons{T}) where {T}
   for c in listReverse(lst1)
     lst2 = cons(c, lst2)
@@ -74,7 +100,7 @@ function listGet(lst::Cons{T}, index #= one-based index =#::Int)::T where {T}
 end
 
 """
-  listRest for nil is not defined. 
+  listRest for nil is not defined.
   Returns an error.
 """
 function listRest(lst::Nil{T}) where {T}
@@ -95,7 +121,7 @@ function listHead(lst::Nil{T})::T where {T}
 end
 
 
-""" Returns the head of a list. 
+""" Returns the head of a list.
 Time complexity: O(1) """
 function listHead(lst::Cons{T})::T where {T}
   lst.head
@@ -118,12 +144,18 @@ Base.string(lst::Nil) = let
   "{}"
 end
 
-Base.string(lst::Cons) = let
-  res = "{"
-  for e in lst
-    res *= string(e) * "<-" 
+
+Base.string(lst::Cons{T}) where {T}= let
+  local res = "list{$(T)}["
+  local N = length(lst)
+  for (i,e) in enumerate(lst)
+    if i != N
+      res *= string(e) * ","
+    else
+      res *= string(e)
+    end
   end
-  res *= "nil}"
+  res *= "]"
 end
 
 """
@@ -136,6 +168,45 @@ end
 """ A list consisting of a nil element is empty """
 function listEmpty(lst::Nil{T})::Bool where {T}
   true
+end
+
+"""
+```
+Base.first(lst::Nil)
+```
+A list consisting only of nil is nil.
+"""
+Base.first(lst::Nil) = let
+  lst
+end
+
+
+"""
+Both first and last is the same for a nil list.
+"""
+Base.last(lst::Nil) = let
+  lst
+end
+
+"""
+Last on a cons cell is the tail.
+"""
+Base.last(lst::Cons) = let
+  lst.tail
+end
+
+"""
+```
+Base.first(lst::Cons{T})
+```
+Returns the head of the list
+"""
+Base.first(lst::Cons) = let
+  lst.head
+end
+
+Base.show(io::IO, ::MIME"text/plain", lst::List) = begin
+  print(io, string(lst))
 end
 
 export listAppend
